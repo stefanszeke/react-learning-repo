06144e3640312d0d9c4757b17ed3bf058d3227a7
Stefan Szekely <sekestefan@gmail.com>
2022-11-10 12:39:27 +0100

    update: counter with effects

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,16 +4,24 @@ import clsx from "clsx";
 
 export default function Counter(props) {
   const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
+  const [count3, setCount3] = useState(0);
   const { className, ...rest } = props;
   const classes = clsx("ui-counter", className);
 
-  const handleIncrementClick = () => (count >= 9) ? setCount(0) : setCount(count + 1)
-  const handleDecrementClick = () => (count <= 0) ? setCount(9) : setCount(count - 1)
+  const handleIncrementClick = () => { (count >= 9) ? setCount(0) : setCount(count + 1); setCount2(count) }
+  const handleDecrementClick = () => { (count <= 0) ? setCount(9) : setCount(count - 1); setCount2(count) }
   
   function handleAddClick() {
     setCount(prev => prev + 1);
     setCount(prev => prev + 1);
     setCount(prev => prev + 1);
+    setCount2(count)
+  }
+
+  function handleResetClick() {
+    setCount(0);
+    setCount2(count)
   }
 
   useEffect(() => {
@@ -25,18 +33,29 @@ export default function Counter(props) {
     }, 2000);
 
     return () => clearInterval(interval);
-
   }, [count])
+
+  useEffect(() => { setCount3(count) }, [count])
+
+
 
   return (
     <div className={classes} {...rest}>
       <h3>Counter</h3>
       <p>( document.title )</p>
-      <h2>{count}</h2>
+      <div className="ui-counters">
+        <h3>counter: </h3>
+        <h2>{count}</h2>
+        <h3>a little behind: </h3>
+        <h2>{count2}</h2>
+        <h3>useEffect: </h3>
+        <h2>{count3}</h2>
+      </div>
       <div className='ui-counter-buttons'>
         <button onClick={handleIncrementClick}>+</button>
         <button onClick={handleDecrementClick}>-</button>
         <button onClick={handleAddClick}>add3</button>
+        <button onClick={handleResetClick}>reset</button>
       </div>
     </div>
   );
